8acc4e3d72cac6a40aea45d439bfbf15d6df62a8
Cristiano Soares <1191816@isep.ipp.pt>
2024-05-13 22:21:22 +0100

    #10 Create delivery bootstrap

--- Deliverables/BackendAPI/src/main/java/pt/isep/desofs/m1a/g1/model/delivery/Delivery.java
+++ Deliverables/BackendAPI/src/main/java/pt/isep/desofs/m1a/g1/model/delivery/Delivery.java
@@ -1,11 +1,15 @@
 package pt.isep.desofs.m1a.g1.model.delivery;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Delivery {
 
     private UUID id;
@@ -13,4 +17,11 @@ public class Delivery {
     private LocalDate deliveryDate;
     private Double weight;
     private String warehouseId;
+
+    public Delivery(long identifier, LocalDate date, double weight, String warehouseId) {
+        this.identifier = identifier;
+        this.deliveryDate = date;
+        this.weight = weight;
+        this.warehouseId = warehouseId;
+    }
 }
