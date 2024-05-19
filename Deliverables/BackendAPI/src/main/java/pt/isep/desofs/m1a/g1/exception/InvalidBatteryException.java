452cee2d94f0f251a0e6215592861d02533beedc
Alexandra Leite [1170541] <1170541@isep.ipp.pt>
2024-05-19 15:49:00 +0100

    #11 Added validations on Truck

---
+++ Deliverables/BackendAPI/src/main/java/pt/isep/desofs/m1a/g1/exception/InvalidBatteryException.java
@@ -0,0 +1,8 @@
+package pt.isep.desofs.m1a.g1.exception;
+
+public class InvalidBatteryException extends RuntimeException{
+
+    public InvalidBatteryException(String message) {
+        super(message);
+    }
+}
